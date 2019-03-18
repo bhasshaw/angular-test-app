8003967ebf8d2aae8e1a50481121ce4ac824d36d
Bryce Hasshaw <bhasshaw@gmail.com>
2019-03-17 21:18:43 -0700

    remove nav tabs / add bootstrap

--- angular-test-app/src/app/app.module.ts
+++ angular-test-app/src/app/app.module.ts
@@ -1,23 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { AboutComponent } from './about/about.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ContactComponent } from './contact/contact.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    AboutComponent,
-    ProjectsComponent,
-    ContactComponent,
-    NgbModule
+    NavComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
