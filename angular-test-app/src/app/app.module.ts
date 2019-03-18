415a4fedc247762e001fc39a708be3955cf93b8a
Bryce Hasshaw <bhasshaw@gmail.com>
2019-03-17 21:22:19 -0700

    added main comp

--- angular-test-app/src/app/app.module.ts
+++ angular-test-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    MainComponent
   ],
   imports: [
     NgbModule,
